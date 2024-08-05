2fa4f88144934bb1a215f507f356a52442fb68c7
beer-alchimist <profel.game@yandex.ru>
2024-08-06 00:48:12 +0300

    adptive container menu

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ var chat_id = ['chat_1','chat_2','chat_3','chat_4'];
 function toggleNavbar() {
     const navbar = document.getElementById('navbar');
     navbar.classList.toggle('active');
-}
+};
 
 function open_chat(id){
     chat_id.forEach(row=>{
@@ -13,4 +13,8 @@ function open_chat(id){
             document.getElementById(row).style.display = 'none';
         }
     });
-}
+};
+
+function close_chat(id){
+    document.getElementById(id).style.display = 'none';
+};
