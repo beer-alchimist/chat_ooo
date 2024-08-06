a661f35ddf54ca265f6ba282c2ceee1d916b6087
beer-alchimist <profel.game@yandex.ru>
2024-08-06 07:37:45 +0300

    project surself

--- src/js/chat_func.js
+++ src/js/chat_func.js
@@ -1,5 +1,4 @@
-var dialogs_id=['filter_new', 'filter_old','filter_all'];
-
+var dialogs_id=['filter_new', 'filter_old','filter_all','filter_new_2', 'filter_old_2','filter_all_2'];
 /*var dialog_filter_message_id = {
     dialogs_id:['filter_new', 'filter_old','filter_all'],
     chat_id:['notes','comment','history_caht']
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-var chat_id = ['chat_1','chat_2','chat_3','chat_4'];
+var chat_id = ['chat_1','chat_2','chat_3','chat_4','chat_1_1','chat_2_2','chat_3_3','chat_4_4'];
 
 function toggleNavbar() {
     const navbar = document.getElementById('navbar');
@@ -13,6 +13,22 @@ function open_chat(id){
             document.getElementById(row).style.display = 'none';
         }
     });
+};
+
+function open_chat_2(id){
+    document.getElementById('dialogs_2').style.display= 'none';
+    chat_id.forEach(row=>{
+        if (id == row) {
+            document.getElementById(id).style.display = 'block';
+        } else {
+            document.getElementById(row).style.display = 'none';
+        }
+    });
+};
+
+function close_chat_2(id){
+    document.getElementById(id).style.display = 'none';
+    document.getElementById('dialogs_2').style.display = 'block';
 };
 
 function close_chat(id){
